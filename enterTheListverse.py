18e1323a2cbddff1648ff35b34260fb8ae5c0abb
Jasminedm <mooredjasmine01@gmail.com>
2023-10-12 12:14:40 -0400

    enterTheListverse  practice

---
+++ enterTheListverse.py
@@ -0,0 +1,10 @@
+fav_foods = ["pickle", "cake", "mango", "pineapple", "honeydew"]
+
+print(fav_foods[2:3])
+print(fav_foods[3:4])
+
+fav_foods.append("cantelope")
+print(fav_foods)
+
+rm = fav_foods.pop(1)
+print(fav_foods)
